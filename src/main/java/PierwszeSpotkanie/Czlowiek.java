df3b7f2c02ace3f4a1f1ee50006f0f6fe6392e27
Basia258 <barbara.barzycka@gmail.com>
2018-09-18 21:39:18 +0200

    init repo

---
+++ src/main/java/PierwszeSpotkanie/Czlowiek.java
@@ -0,0 +1,11 @@
+package PierwszeSpotkanie;
+
+public class Czlowiek {
+
+    //  Dw sleshe to est komentarz. Tutaj moge pisac sobie po polsku i pieroloy tez asdsfkasdflashfasdhhfads
+    //cecehy fizyczne - czyli pola
+
+    private String name;
+    private int wiek;
+
+}
---
+++ src/main/java/PierwszeSpotkanie/MainCzlowiek.java
@@ -0,0 +1,13 @@
+package PierwszeSpotkanie;
+
+public class MainCzlowiek {
+
+    public static void main(String[] args) {
+
+        new Czlowiek();
+
+        Czlowiek jan = new Czlowiek();
+
+
+    }
+}
