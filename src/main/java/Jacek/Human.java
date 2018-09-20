7153f34b7ee5000ede754e233ac5644ecc6bebf0
Jacek Mroczek <jacko.mds@interia.pl>
2018-09-20 20:45:23 +0200

    first problem in my table

--- src/main/java/Jacek/Human.java
+++ src/main/java/Jacek/Human.java
@@ -8,4 +8,19 @@ public class Human {
     private float weight;
     private boolean isAdult;
 
+    public String introduceMyself(String name) {
+        return "My name is" + name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public boolean isAdult() {
+        return isAdult;
+    }
 }
---
+++ src/main/java/Jacek/MainHuman.java
@@ -0,0 +1,8 @@
+package Jacek;
+
+public class MainHuman {
+
+    public static void main(String[] args) {
+        String Jacek = newHuman()
+    }
+}
