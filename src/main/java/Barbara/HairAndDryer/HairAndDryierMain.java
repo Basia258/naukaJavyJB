83b38930426d540084cb7d9ad14ee2dc1e5003b2
Pawel Gondek <Pawel.Gondek@sabre.com>
2018-10-05 17:59:52 +0200

    create HairAndDryer package with content

---
+++ src/main/java/Barbara/HairAndDryer/HairAndDryierMain.java
@@ -0,0 +1,29 @@
+package Barbara.HairAndDryer;
+
+public class HairAndDryierMain {
+
+    public static void main(String[] args) {
+
+
+        // Tworze wlosy, ktore nie sa suche
+        MyHair myHair = new MyHair(false);
+
+        // Tworze obiekt suszarki
+        Dryer dryer = new Dryer();
+
+        System.out.println("Pierwsze suszenie sie rozpoczyna:");
+
+        //wywoluje na obiekcie suszarki metody 'dry'. Czyli chece wysuzyc wlosy,  ktore stworzylem w 9 linijce. Te stworzone wlosy nie sa suche wiec obiekt
+        // dryer powinien mi je wysuszyc
+
+        dryer.dry(myHair);
+        System.out.println(""); //zrobienie 'entera' w konsoli by sie lepiej czytalo
+
+
+        //po suszeniu wlosy powinny byc suche. Sprawdzimy to! Jak? Ano sprobujmy jeszcze raz wysuszyc te same wlosy
+
+
+        System.out.println("Drugie suszenie sie rozpoczyna");
+        dryer.dry(myHair);
+    }
+}
---
+++ src/main/java/Barbara/HairAndDryer/MyHair.java
@@ -0,0 +1,25 @@
+package Barbara.HairAndDryer;
+
+public class MyHair {
+
+    private boolean isDry;
+
+    //konstruktor//
+
+    public MyHair(boolean isDry) {
+        this.isDry = isDry;
+    }
+    //getter//
+
+    public boolean isDry() {
+        return this.isDry;
+    }
+
+    //setter//
+
+    public void setDry(boolean isDry) {
+        this.isDry = isDry;
+    }
+
+
+}
