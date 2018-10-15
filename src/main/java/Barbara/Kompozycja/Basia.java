921d7687fbb3c580bc301d061fcc8a279e764ed4
Basia258 <barbara.barzycka@gmail.com>
2018-10-15 23:15:00 +0200

    BandJ

---
+++ src/main/java/Barbara/Kompozycja/Basia.java
@@ -0,0 +1,12 @@
+package Barbara.Kompozycja;
+
+public class Basia {
+
+    private Jacek kochany;
+    public void smiejSie () {
+
+        this.kochany.smiejSie();
+    }
+
+    public void setKochany(Jacek kochany) {this.kochany = kochany;}
+}
