a9f4587d24d6625af3222464920e8bbd09f563e9
Jacek Mroczek <jacko.mds@interia.pl>
2018-10-23 22:36:44 +0200

    algorytm 1 and 2

---
+++ src/main/java/Pawel/algorytm1/Ticket.java
@@ -0,0 +1,18 @@
+package Pawel.algorytm1;
+
+public class Ticket {
+
+    // default -> false
+    private boolean isValidate;
+
+
+    public boolean isValidate() {
+        return isValidate;
+    }
+
+    public void validateTicket()
+    {
+        this.isValidate = true;
+    }
+
+}
